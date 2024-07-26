7c5e89055dcba46b672c42aa400379d4bc58945f
Evanev7 <evanev7@gmail.com>
2024-07-26 18:56:57 +0100

    Moving on to the 2023/275 spec
    big changes all around!

--- BBS.py
+++ BBS.py
@@ -1,12 +1,18 @@
 from random import randint
+from functools import reduce
 
 RETRY_LIMIT = 50
+
+#=======================================================================#
+#= Can be swapped out with a type-3 bilinear group and it's operations =#
+#=======================================================================#
+
+#============  The type-1 bilinear group (p, Zp, Zp, Zp, *) ============#
 # Curve order for BLS12_381
 p = 52435875175126190479447740508185965837690552500527637822603658699938581184513
 # Generators. We're taking G1 = G2 = G12 = Z_p, e(x,y) = (x*y) % p
 G1 = 1
 G2 = 1
-G12 = 1
 
 def mult(a,b):
     return (a*b) % p
@@ -27,149 +33,87 @@ def pairing(a,b):
 # p = cv.curve_order
 
 
-
-def totally_secure_cryptographic_hash_1(n):
-    seed = 612789
+# Used to sample random elements from G1.
+def totally_secure_cryptographic_hash(n, seed=612789):
     g_n = mult(G1, pow(seed + n + 1,-1,p))
-
     return g_n
 
-
-def totally_secure_cryptographic_hash_2(n):
-    seed = 41527809
-    g_n = mult(G2, pow(seed + n + 1,-1,p))
-
-    return g_n
-
-def totally_secure_cryptographic_hash_3(n):
-    seed = 2160421
-    g_n = mult(G2, pow(seed + n + 1,-1,p))
-    return g_n
 
 class Signature:
-    def __init__(self, A,e,s):
+    def __init__(self, A,e):
         self.A = A
         self.e = e
-        self.s = s
-class Params:
-    def __init__(self, g, h, u):
-        self.g = g
-        self.h = h
-        self.u = u
 
+class Params:
+    def __init__(self, h):
+        self.h = h
+
+# Generates public parameters and verifies computation.
 class TrustedPublicAuthority:
     @staticmethod
-    def GSetup(max_messages = 100) -> Params:
+    def GGen(max_messages = 100) -> Params:
         # our totally cryptographically secure hash function ===============================================
-        g = list(map(totally_secure_cryptographic_hash_1, range(0, max_messages+2)))
-        h = list(map(totally_secure_cryptographic_hash_2, range(0, max_messages+2)))
-        u = list(map(totally_secure_cryptographic_hash_3, range(0, max_messages+2)))
-        return Params(g, h, u)
-
-
-class GM:
-    def __init__(self, params = TrustedPublicAuthority.GSetup(max_messages=100)) -> None:
-        self.Reg = []
-        self.GKGen()
-        self.g = params.g
-        self.h = params.h
-        self.u = params.u
-
-
-    def GKGen(self) -> None:
-        self.secret_key = randint(0,p-1) #====================================================================
-        self.public_key = mult(G2, self.secret_key)
-    
-    
-    def sign(self, messageList: list[int]) -> Signature:
-        salt_s = randint(0, p-1)
-        for i in range(RETRY_LIMIT):
-            try:
-                salt_e = randint(0, p-1) # could be hash
-                exp_inv = pow(salt_e + self.secret_key, -1, p)
-                break
-            except ValueError:
-                continue
         
-        # Start off A with G1+s*g0
-        progress_A = add(G1, mult(self.g[0], salt_s)) 
-        for i, message in enumerate(messageList):
-            # then multiply by m1*g1+m2*g2 etc.
-            progress_A = add(progress_A, mult(self.g[i+1], message)) 
-        A = mult(progress_A, exp_inv)
-        assert(progress_A == mult(A, salt_e + self.secret_key))
+        h = list(map(totally_secure_cryptographic_hash, range(0, max_messages+2)))
+        return Params(h)
 
-        return Signature(A, salt_e, salt_s) 
-
-
-    def verify(self, signature: Signature, messageList: list[int]) -> int:
+    @staticmethod
+    def verify(params: Params, public_key: int, signature: Signature, messageList: list[int]) -> int:
         # compute w + e*G2 = (SK+e)*G2
-        term_1 = add(self.public_key, mult(G2,signature.e))
+        term_1 = add(public_key, mult(G2,signature.e))
 
-        # Start off A with G1+s*g0
-        term_4 = add(G1, mult(self.g[0], signature.s))
-        for i, message in enumerate(messageList): 
-            # then multiply by m1*g1+m2*g2 etc.
-            term_4 = add(term_4, mult(self.g[i+1], message))
+        # Start off A with G1
+        term_4 = reduce(add, map(mult, params.h, messageList), G1) 
 
         return pairing(term_1, signature.A) == pairing(G2, term_4)
 
 
-    def _write(self, entry, mpk) -> None:
-        self.Reg.append((len(self.Reg), entry, mpk))
+class GM:
+    def __init__(self, params = TrustedPublicAuthority.GGen(max_messages=100)) -> None:
+        self.GKGen()
+        self.params = params
+
+
+    def GKGen(self) -> None:
+        self.secret_key = randint(1,p-1) #====================================================================
+        self.public_key = mult(G2, self.secret_key)
+    
+    def gm_sign(self, messageList) -> Signature:
+        # C = G1 + m0 * h0 + m1 * h1 + ...
+        C = reduce(add, map(mult, self.params.h, messageList), G1)
+        return self.sign(self, C)
+
+    def sign(self, C) -> Signature:
+        salt_e, exp_inv = self._generate_invertible_salt()
+         
+        # A = C * 1/(SK+e)
+        A = mult(C, exp_inv)
+        # Validation. Failed on py_ecc
+        assert(C == mult(A, salt_e + self.secret_key))
+
+        return Signature(A, salt_e) 
+
 
     def _generate_invertible_salt(self) -> tuple[int, int]:
         for i in range(RETRY_LIMIT):
-            salt_e = randint(0,p-1)
-            exp_inv = pow(salt_e + self.secret_key, -1, p)
-            return salt_e, exp_inv
+            try:
+                salt_e = randint(0,p-1)
+                exp_inv = pow(salt_e + self.secret_key, -1, p)
+                return salt_e, exp_inv
+            except ValueError:
+                continue
+        
         raise ValueError(f"No salt found in {RETRY_LIMIT} attempts.")
 
-
-    def join_1(self) -> int:
-        return len(self.Reg)
-    
-    def join_2(self, Cprime, proof) -> int:
-        #if verify(proof):
-        self.tempCprime = Cprime
-        self.tempsprimeprime = randint(0, p-1)
-        return self.tempsprimeprime
-
-    def join_3(self, entry, proof) -> int:
-        # if verify(proof):
-        C = self.tempCprime + self.tempsprimeprime * self.g[1]
-        salt_e, exp_inv = self._generate_invertible_salt()
-        A = exp_inv * (self.g[0] + C)
-
-        self._write(entry, salt_e)
-        return (A, salt_e, self.tempsprimeprime) 
-
 class User:
-    def __init__(self, params = TrustedPublicAuthority.GSetup(max_messages=100)) -> None:
+    def __init__(self, params = TrustedPublicAuthority.GGen(max_messages=100)) -> None:
         self.params = params
         
     
-    def join_1(self, id: int) -> tuple:
-        self.id = id
-        self.sprime = randint(0, p-1)
-        self.t = randint(0, p-1)
-        self.x = randint(0, p-1)
-        Cprime = self.sprime * self.params.g[1] + self.t * self.params.g[2] + self.x * self.params.g[3]
-        proof = 0 #======================================================================================
-        return Cprime, proof
-    
-    def join_2(self, sprimeprime: int) -> tuple:
-        self.s = self.sprime + sprimeprime
-        entry = (self.id, self.x * self.params.u[0])
-        proof = 0 #======================================================================================
-        return entry, proof
-
-    def join_3(self, A: int, e: int, sprimeprime: int, gm: GM): #GM here for public parameters.
-        term_3 = self.params.g[0] + self.s * self.params.g[1] + self.t * self.params.g[2] + self.x * self.params.g[3]
-        if pairing(A, gm.public_key + e * G2) != pairing(term_3, G2):
-            raise ValueError("Invalid personal signature received.")
-        self.public_key = e
-        self.private_key = (A,self.s,self.t,self.x)
+    def compute_commitment(self, messageList: list[int]) -> int:
+        # C = G1 + m0 * h0 + m1 * h1 + ...
+        C = reduce(add, map(mult, self.params.h, messageList), G1)
+        return C
 
 
 class InsecureChannel:
@@ -177,13 +121,11 @@ class InsecureChannel:
         self.leaked_data = []
 
 
-    def join(self, gm: GM, user: User):
-        user_id = gm.join_1()
-        Cprime, proof = user.join_1(user_id)
-        sprimeprime = gm.join_2(Cprime, proof)
-        entry, proof_2 = user.join_2(sprimeprime)
-        (A,salt_e, sprimeprime) = gm.join_3(entry, proof_2)
-        user.join_3(A, salt_e, sprimeprime, gm)
+    def user_sign(self, user: User, gm: GM, messageList):
+        C = user.compute_commitment(messageList)
+        sig = gm.sign(C)
 
         self.leaked_data.append([locals()])
+
+        return sig 
 
--- tests.py
+++ tests.py
@@ -7,24 +7,26 @@ num_users = 1000
 import BBS
 
 def test_message_signing(num_attempts, num_messages):
-        
+    
     num_true_positives = 0
     num_false_positives = 0
     num_true_negatives = 0
     num_false_negatives = 0
 
-    params = BBS.TrustedPublicAuthority.GSetup(max_messages=num_messages)
+    params = BBS.TrustedPublicAuthority.GGen(max_messages=num_messages)
     for _ in range(num_attempts):
         gm = BBS.GM(params=params)
+        user = BBS.User(params=params)
+        channel = BBS.InsecureChannel()
         msgs = [randint(0,10000) for _ in range(num_messages)]
         false_msgs = [randint(0,10000) for _ in range(num_messages)]
-        sig = gm.sign(msgs)
+        sig = channel.user_sign(user, gm, msgs)
         
-        if gm.verify(sig, msgs) == True:
+        if BBS.TrustedPublicAuthority.verify(params, gm.public_key, sig, msgs) == True:
             num_true_positives += 1
         else:
             num_false_negatives += 1
-        if gm.verify(sig, false_msgs) == False:
+        if BBS.TrustedPublicAuthority.verify(params, gm.public_key, sig, false_msgs) == False:
             num_true_negatives += 1
         else:
             num_false_negatives += 1 
@@ -38,6 +40,8 @@ def test_message_signing(num_attempts, num_messages):
     assert(num_false_positives == 0)
     assert(num_false_negatives == 0)
 
+    return channel
+
 def user_join_sequence(num_users):
     gm = BBS.GM()
     channel = BBS.InsecureChannel()
@@ -46,7 +50,7 @@ def user_join_sequence(num_users):
         user = BBS.User()
         channel.join(gm=gm, user=user)
     
-    print(f"{len(gm.Reg)} users successfully registered with GM")
+    print(f"        {len(gm.Reg)} users successfully registered with GM")
 
     # channel.leaked_data contains a reference to any data leaked in the insecure channel.
 
@@ -55,4 +59,4 @@ def user_join_sequence(num_users):
 
 if __name__ == "__main__":
     test_message_signing(num_attempts=num_attempts, num_messages=num_messages)
-    user_join_sequence(num_users=num_users)
+#    user_join_sequence(num_users=num_users)
